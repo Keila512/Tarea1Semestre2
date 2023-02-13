c6d84b2d7ff04d5aad688a40460a5d394018185a
Angel Zurita <af3392976@gmail.com>
2023-02-12 22:50:29 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Operaciones.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Operaciones
+    {
+        private int numero1;
+        private int numero2;
+        private int numero3;
+        private int numero4;
+        private int numero5;
+
+        public int Numero1 { get { return numero1; } set { numero1 = value; } }
+        public int Numero2 { get { return numero2; } set { numero2 = value; } }
+        public int Numero3 { get { return numero3; } set { numero3 = value; } }
+        public int Numero4 { get { return numero4; } set { numero4 = value; } }
+        public int Numero5 { get { return numero5; } set { numero5 = value; } }
+        
+        public Operaciones()
+        {
+            numero1 = 0;
+            numero2 = 0;
+            numero3 = 0;
+            numero4 = 0;
+            numero5 = 0;
+        }
+
+        public Operaciones(int numero1, int numero2, int numero3, int numero4, int numero5)
+        {
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+            this.numero3 = numero3;
+            this.numero4 = numero4;
+            this.numero5 = numero5;
+        }
+
+        public int Sumar()
+        {
+            return numero1 + numero2 + numero3 + numero4 + numero5;
+        }
+
+        public int Resta()
+        {
+            return numero1 - numero2 - numero3 - numero4 - numero5;
+        }
+
+        public  double Promedio()
+        {
+            return (numero1 + numero2 + numero3 + numero4 + numero5)*1.0 / 5;
+        }
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static int x = 0;
+        static void Main(string[] args)
+        {
+            Operaciones a = new Operaciones();
+            a.Numero1 = 5;
+            a.Numero5 = 1;
+            Operaciones b = new Operaciones();
+            b.Numero1 = 8;
+            b.Numero5 = 4;
+            int suma = a.Sumar();
+            int sumab = a.Numero1 + a.Numero5;
+            int sumac = SumarProgram(a.Numero1, a.Numero5);
+            Console.WriteLine(x);
+            sumarP(a.Numero1, a.Numero5);
+            Console.WriteLine(x);
+            //Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+
+        private static int SumarProgram(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+        private static void sumarP(int numero1, int numero2)
+        {
+            x = numero1 + numero2;
+        }
+    }
+}
